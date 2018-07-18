45025861cd29e38f68e81b3f8653dae1d55662c8
Charlotte Dann <ask@pouretrebelle.com>
2018-07-18 22:46:40 +0100

    :sparkles: Use Canny for edge detection

--- src/vision.js
+++ src/vision.js
@@ -1,15 +1,20 @@
 import jsfeat from 'jsfeat';
 
-const images = Array.from(document.getElementsByTagName('img'));
+const images = Array.from(document.getElementsByTagName('img')).filter(
+  (img, i) => i % 2
+);
+
+const removeDuplicates = (arr) => {
+  let s = new Set(arr);
+  let it = s.values();
+  return Array.from(it);
+};
 
 const corners = (image) => {
   const canvas = document.createElement('canvas');
   canvas.width = 500;
   canvas.height = 500;
   const ctx = canvas.getContext('2d');
-
-  ctx.fillStyle = 'rgb(0,255,0)';
-  ctx.strokeStyle = 'rgb(0,255,0)';
 
   const img_u8 = new jsfeat.matrix_t(
     canvas.width,
@@ -31,28 +36,99 @@ const corners = (image) => {
   var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   jsfeat.imgproc.grayscale(imageData.data, canvas.width, canvas.height, img_u8);
 
-  var count = jsfeat.fast_corners.detect(img_u8, corners, 5);
+  let options = {
+    blur_radius: 3,
+    low_threshold: 5,
+    high_threshold: 25,
+  };
+  var r = options.blur_radius | 0;
+  var kernel_size = (r + 1) << 1;
+
+  jsfeat.imgproc.gaussian_blur(img_u8, img_u8, kernel_size, 0);
+
+  var count = jsfeat.imgproc.canny(
+    img_u8,
+    img_u8,
+    options.low_threshold | 0,
+    options.high_threshold | 0
+  );
 
   var data_u32 = new Uint32Array(imageData.data.buffer);
 
-  render_corners(corners, count, data_u32, canvas.width);
+  let hits = [];
+
+  for (let row = 0; row < img_u8.rows; row++) {
+    let start = row * img_u8.cols;
+    let adder = 1;
+    let i = start;
+    let found = false;
+
+    // left to right
+    while (i < start + img_u8.cols && !found) {
+      if (img_u8.data[i] & 0xff) {
+        found = true;
+        hits.push(i);
+      }
+      i += adder;
+    }
+
+    // right to left
+    i = start + img_u8.cols * adder;
+    found = false;
+    while (i > start && !found) {
+      if (img_u8.data[i] & 0xff) {
+        found = true;
+        hits.push(i);
+      }
+      i -= adder;
+    }
+  }
+
+  for (let col = 0; col < img_u8.cols; col++) {
+    let start = col;
+    let adder = img_u8.rows;
+    let i = start;
+    let found = false;
+
+    // top to bottom
+    while (i < start + img_u8.rows * adder && !found) {
+      if (img_u8.data[i] & 0xff) {
+        found = true;
+        hits.push(i);
+      }
+      i += adder;
+    }
+
+    // bottom to top
+    i = start + img_u8.rows * adder;
+    found = false;
+    while (i > start && !found) {
+      if (img_u8.data[i] & 0xff) {
+        found = true;
+        hits.push(i);
+      }
+      i -= adder;
+    }
+  }
+
+  hits = removeDuplicates(hits);
+
+  drawHits(hits, img_u8, data_u32);
+
   ctx.putImageData(imageData, 0, 0);
 
   image.src = canvas.toDataURL();
 };
 
-function render_corners(corners, count, img, step) {
-  var pix = (0xff << 24) | (0x00 << 16) | (0xff << 8) | 0x00;
-  for (var i = 0; i < count; ++i) {
-    var x = corners[i].x;
-    var y = corners[i].y;
-    var off = x + y * step;
-    img[off] = pix;
-    img[off - 1] = pix;
-    img[off + 1] = pix;
-    img[off - step] = pix;
-    img[off + step] = pix;
+const drawHits = (hits, img_u8, data_u32) => {
+  // make everything black
+  let j = img_u8.cols * img_u8.rows;
+  while (--j >= 0) {
+    data_u32[j] = 0xff000000;
   }
-}
+  // make all the hits white
+  hits.forEach((i) => (data_u32[i] = 0xffffffff));
+};
 
+// corners(images[0]);
 images.forEach((image) => corners(image));
